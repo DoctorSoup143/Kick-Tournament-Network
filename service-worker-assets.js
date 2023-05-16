d38278fa1209dbd63d60760168cd49e71d9a1a74
DoctorSoup143 <sdrobish4@gmail.com>
2023-05-15 20:33:46 -0400

    Updated SRC field for fonts

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -169,7 +169,7 @@
       "url": "css\/animations.css"
     },
     {
-      "hash": "sha256-DxaZuPwBY3CHMzC7BmRvnomQq\/Naf7\/HSh0lbFYmMAM=",
+      "hash": "sha256-2Cbn4N7Db2Ln0ka2nMRci0we1mRCzrOfpH6z042F0XQ=",
       "url": "css\/app.css"
     },
     {
@@ -453,5 +453,5 @@
       "url": "sample-data\/weather.json"
     }
   ],
-  "version": "7J2SNVh6"
+  "version": "cGFQl7tI"
 };
--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: 7J2SNVh6 */
+/* Manifest version: cGFQl7tI */
